5aab52ad4dfb82a8ab8905aa8e7d97aa63436669
Mason Galland <masongalland@me.com>
2017-07-06 11:58:36 -0600

    made get requests to get enemies and troops

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import {getEnemies} from './services/enemies';
+import {getTroops} from './services/troops'
 import './styles/App.css';
 
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      enemies: [],
+      troops: [],
+      defensesArray: []
+    };
 
+    this.seeEnemies = this.seeEnemies.bind(this);
+  }
+
+  componentDidMount() {
+    getTroops().then(troops => {
+      this.setState({troops});
+    })
+  }
+  
+  
 
 
   seeEnemies() {
+    getEnemies().then(enemies => {
+      this.setState({enemies});
+    })
   }
 
   callTroops() {
+
   }
 
   recruitTroop() {
@@ -26,8 +50,29 @@ class App extends Component {
 
   render() {
 
+    const troops = this.state.troops.map((troop, i) => (
+      <li className='troop' key={i}>{troop.recruit}</li>
+    ))
 
-    const message = this.state && this.state.armiesArray.length < 1 ? "ALL CLEAR" : "";
+    const enemies = this.state.enemies.map((enemy, i) => {
+      return (
+        <ul className='army'>
+          <h3>Enemy Army #{enemy.id}: {enemy.name}</h3>
+
+          <div className = 'leader'>
+            {enemy.leader}
+          </div>
+
+          <ul className='minions'>
+            {enemy.minions.map((minion, i) => (<li className='minion' key={i}>{minion.type}</li>))}
+          </ul>
+
+
+        </ul>
+      )
+    })
+
+    const message = this.state && this.state.enemies.length < 1 ? "ALL CLEAR" : "";
 
     return (
       <div className="App">
@@ -37,7 +82,7 @@ class App extends Component {
           <h1>Enemies at our gate!</h1>
           <h2>Prepare our defenses!</h2>
           <div className="defenses">
-            <div className="defense" id="sentry">Sentry<span className="instructions">Click here to see approaching enemies!</span></div>
+            <div className="defense" id="sentry" onClick={this.seeEnemies}>Sentry<span className="instructions">Click here to see approaching enemies!</span></div>
             <div className="defense" id="captain">Captain<span className="instructions">Fill out Request Form below to recruit new troop!</span></div>
             <div className="defense" id="wizard">Wizard<span className="instructions">Click directly on a minion to cast a spell!</span></div>
             <div className="defense" id="ballista">Ballista<span className="instructions">Blast enemy leader to disperse army!</span></div>
@@ -59,14 +104,14 @@ class App extends Component {
         </div>
 
         <ul className="troops">
-          
+          {troops}
         </ul>
 
         <h1 id="message">{message}</h1>
 
         {/* Enemy Armies */}
         <div className="enemies">
-
+          {enemies}
         </div>
       </div>
     );
